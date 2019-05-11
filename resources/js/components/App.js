efa2afe7cad178159343a6ff3b4d88dff73d23f1
Habib <habibyafi45@gmail.com>
2019-05-11 18:22:01 +0700

    fetch data

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -4,6 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 
 export default class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            homestays: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/homestay')
+            .then(response => {
+                return response.json();
+            })
+            .then(homestays => {
+                this.setState({ homestays });
+            })
+        fetch()
+            .then(response => {
+                return response.json();
+            })
+    }
+
     render() {
         
         return (
